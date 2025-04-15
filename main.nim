a31b0e8f1c58f845196b8245718955da66616350
Coconut0211 <ilyaserov02@gmail.com>
2025-04-15 22:25:52 +0300

    3

--- main.nim
+++ main.nim
@@ -5,7 +5,7 @@ import parsecsv
 proc toJson(data: seq[tuple[k, v: string]]): JsonNode =
   result = newJObject()
   for item in data:
-    result.add(item.k, %* item.v)
+    result[item.k] = %* item.v
 
 proc readCSV(filename: string): seq[JsonNode] =
   var csv: CsvParser
@@ -19,7 +19,7 @@ proc loadCSV(filename: string) {. thread .} =
   echo (%* readCSV(filename)).pretty
 
 when isMainModule:
-  let files = walkDirRec("data").toSeq.filter(proc(x: string): bool = x.splitFile.ext  == ".csv")
+  let files = walkFiles("data" / "*.csv").toSeq
   var threads =  newSeq[Thread[string]](len(files))
   for i in threads.low .. threads.high:
     createThread(threads[i], loadCSV, files[i])
